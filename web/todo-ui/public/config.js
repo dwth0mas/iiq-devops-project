216d6c1eee77438b6c4dfc2d209b769d113dda35
Dylan Thomas <dylanthomas@Dylans-MacBook-Pro.local>
2024-09-10 00:23:39 -0600

    reseting git cache pt 4

--- web/todo-ui/public/config.js
+++
@@ -1,1 +0,0 @@
-var BASE_URL = 'http://iiqdevopsproject.com/todoitems'
--- web/todo-ui/src/settings.js
+++
@@ -1,2 +0,0 @@
-export const testUrl = 'http://localhost:5050/todoitems'
-export const clusterApiUrl = 'http://iiqdevopsproject.com/todoitems'
